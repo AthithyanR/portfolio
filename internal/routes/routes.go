84fa63da082e321c4f0f6ddf14381d4b56484efd
Athithyan Rengasamy <athithyan@crystal.local>
2023-12-08 21:33:27 +0530

    remove nasty stuff

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -2,12 +2,9 @@ package routes
 
 import (
 	"html/template"
-	"log"
 	"time"
 
-	"github.com/AthithyanR/portfolio/internal/database"
 	"github.com/fasthttp/router"
-	realip "github.com/ferluci/fast-realip"
 	"github.com/valyala/fasthttp"
 )
 
@@ -29,50 +26,50 @@ func root(ctx *fasthttp.RequestCtx) {
 	tmpl := template.Must(template.ParseFiles(templatesDir + "/index.html"))
 
 	tmpl.Execute(ctx, map[string]interface{}{
-		"techIKnow":    GetTechIKnow(),
-		"catFact":      GetCatFact(),
-		"chatMessages": GetChatMessages(),
+		"techIKnow": GetTechIKnow(),
+		// "catFact":      GetCatFact(),
+		// "chatMessages": GetChatMessages(),
 	})
 }
 
-func addNewMessage(ctx *fasthttp.RequestCtx) {
-	ctx.Response.Header.Set(contentType, applicationJson)
-	ctx.Response.Header.Set(cacheControl, noCache)
+// func addNewMessage(ctx *fasthttp.RequestCtx) {
+// 	ctx.Response.Header.Set(contentType, applicationJson)
+// 	ctx.Response.Header.Set(cacheControl, noCache)
 
-	name := string(ctx.FormValue("name"))
-	message := string(ctx.FormValue("message"))
+// 	name := string(ctx.FormValue("name"))
+// 	message := string(ctx.FormValue("message"))
 
-	if name == "" || message == "" {
-		log.Println("Invalid message body")
-		ctx.Response.SetStatusCode(500)
-		return
-	}
+// 	if name == "" || message == "" {
+// 		log.Println("Invalid message body")
+// 		ctx.Response.SetStatusCode(500)
+// 		return
+// 	}
 
-	chatMessage := database.ChatMessage{
-		Ip:      realip.FromRequest(ctx),
-		Name:    name,
-		Message: message,
-	}
+// 	chatMessage := database.ChatMessage{
+// 		Ip:      realip.FromRequest(ctx),
+// 		Name:    name,
+// 		Message: message,
+// 	}
 
-	err := CreateChatMessage(&chatMessage)
-	if err != nil {
-		log.Println("Unable to create chat message", err)
-		ctx.Response.SetStatusCode(500)
-		return
-	}
+// 	err := CreateChatMessage(&chatMessage)
+// 	if err != nil {
+// 		log.Println("Unable to create chat message", err)
+// 		ctx.Response.SetStatusCode(500)
+// 		return
+// 	}
 
-	tmpl := template.Must(template.ParseFiles(templatesDir + "/index.html"))
+// 	tmpl := template.Must(template.ParseFiles(templatesDir + "/index.html"))
 
-	tmpl.ExecuteTemplate(ctx, "chatMessages", map[string]interface{}{
-		"chatMessages": []database.ChatMessage{chatMessage},
-	})
-}
+// 	tmpl.ExecuteTemplate(ctx, "chatMessages", map[string]interface{}{
+// 		"chatMessages": []database.ChatMessage{chatMessage},
+// 	})
+// }
 
 func NewRouter() *router.Router {
 	r := router.New()
 
 	r.GET("/", root)
-	r.POST("/new-message", addNewMessage)
+	// r.POST("/new-message", addNewMessage)
 
 	fsHandler := &fasthttp.FS{
 		Root: "static",
